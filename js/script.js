237b57f685edbcbae25d01ef3a3cb6f070484a3d
TacticalVoteScotland <robin@robinwilde.me>
2024-03-04 19:31:59 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,10 +23,7 @@ let graphicsGenerated = 0;
     const action = e.target.action;
     fetch(action, {
       method: 'POST',
-      body: data,
-    })
-    .then(() => {
-      alert("Success!");
+      body: captureData,
     })
   });
 });
